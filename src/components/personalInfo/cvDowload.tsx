0284dfa91948199c2ef5c7a02ac740209cecdf98
GasparKral <147488938+GasparKral@users.noreply.github.com>
2025-12-01 12:02:30 +0100

    Update: add Curriculum to donwload

--- src/components/personalInfo/cvDowload.tsx
+++ src/components/personalInfo/cvDowload.tsx
@@ -1,7 +1,7 @@
 export const CvDownload = ({ text }: { text: string }) => {
     return (
         <a
-            href='/cv.pdf'
+            href='/portafolio/files/Curriculum.pdf'
             target='_blank'
             rel='noopener noreferrer'
             className='p-2 rounded-xs hover:text-primary/60 hover:bg-input/50 active:bg-accent bg-input active:text-primary transition-colors duration-200 flex items-center cursor-pointer '
