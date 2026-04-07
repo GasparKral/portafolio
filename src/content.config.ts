e005b87aa3052f38396b5b9d495f8448d1a89d37
Gaspar <gaspargkral05@gmail.com>
2026-04-07 12:27:39 +0200

    Refactor internalization static generation method

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,48 +1,67 @@
-import { defineCollection, reference, z } from 'astro:content';
-import { glob } from 'astro/loaders';
+import { defineCollection, reference, z } from "astro:content";
+import { glob } from "astro/loaders";
+
+const blogEntries = defineCollection({
+  loader: glob({
+    pattern: "**/*.json",
+    base: "src/content/blogEntries/",
+  }),
+  schema: z.object({
+    title: z.string(),
+    preview_description: z.string(),
+    sections: z
+      .object({
+        title: z.string(),
+        text: z.string(),
+        footer: z.string().optional(),
+      })
+      .array(),
+  }),
+});
 
 const projects = defineCollection({
-    loader: glob({
-        pattern: '**/*.json',
-        base: 'src/content/projects/',
+  loader: glob({
+    pattern: "**/*.json",
+    base: "src/content/projects/",
+  }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    links: z.object({
+      repository: z.string().url().optional(),
+      live: z.string().url().optional(),
     }),
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        links: z.object({
-            repository: z.string().url().optional(),
-            live: z.string().url().optional(),
-        }),
-        technologies: z.array(reference('technologies')).optional(),
-        color: z.string().optional().default('#fafafa'),
-    }),
+    technologies: z.array(reference("technologies")).optional(),
+    color: z.string().optional().default("#fafafa"),
+  }),
 });
 
 export const techScopeEnum = z.enum([
-    'frontend',
-    'backend',
-    'database',
-    'other',
+  "frontend",
+  "backend",
+  "database",
+  "other",
 ]);
 
 const technologies = defineCollection({
-    loader: glob({
-        pattern: '**/**/*.json',
-        base: 'src/content/technologies/',
-    }),
-    schema: z.object({
-        scope: techScopeEnum,
-        name: z.string(),
-        description: z.string(),
-        opinion: z.string().optional(),
-        rate: z.number().multipleOf(0.5).min(0).max(10).default(5),
-        logo: z.string().optional(),
-        ecosystem: z.string(),
-        ecosystem_logo: z.string().optional(),
-    }),
+  loader: glob({
+    pattern: "**/**/*.json",
+    base: "src/content/technologies/",
+  }),
+  schema: z.object({
+    scope: techScopeEnum,
+    name: z.string(),
+    description: z.string(),
+    opinion: z.string().optional(),
+    rate: z.number().multipleOf(0.5).min(0).max(10).default(5),
+    logo: z.string().optional(),
+    ecosystem: z.string(),
+    ecosystem_logo: z.string().optional(),
+  }),
 });
 
 export const collections = {
-    projects,
-    technologies,
+  projects,
+  technologies,
+  blogEntries,
 };
--- src/scripts/in18/hooks.ts
+++ src/scripts/in18/hooks.ts
@@ -1,73 +1,88 @@
 export const languages = {
-    en: 'English',
-    es: 'Spanish',
+  en: "English",
+  es: "Spanish",
 };
 
-export const defaultLanguage = 'es';
+export const defaultLanguage = "es";
 
 export const ui = {
-    en: {
-        'General.DownloadCv': 'Download CV',
-        'General.EmailCopied': 'Email copied to clipboard!',
-        'General.EmailCopyError': 'Failed to copy email.',
-        'Hero.Title':
-            'Welcome to my portfolio!\nIm <b class="font-bold">{name}</b>, <span class="text-amber-300 font-medium">non just any</span> software developer.',
-        'AboutMe.Title':
-            'Some things about <span class="text-accent">me</span>',
-        'AboutMe.Description':
-            "I'm a passionate software developer who loves creating useful and efficient tools. I enjoy learning new technologies and finding ways to optimize my solutions.",
-        'AboutMe.WhatILikeToDo':
-            'I have experience in both web development and desktop/mobile application development. This versatility has helped me discover my love for being a multi-disciplinary developer. I enjoy working on the frontend, applying my UI/UX design knowledge gained through experience, as well as backend development, creating robust and scalable APIs.',
-        'Contact.Title': 'Ways of Contact',
-        'Contact.Social.Title': 'And my socials',
-    },
-    es: {
-        'General.DownloadCv': 'Descarga mi CV',
-        'General.EmailCopied': '¡Email copiado al portapapeles!',
-        'General.EmailCopyError': 'Error al copiar el email.',
-        'Hero.Title':
-            "Bienvenido a mi portafolio.\n<b class='font-bold'>{name}</b>: un <span class='text-accent font-medium'>apasionado</span> del software.",
-        'AboutMe.Title': '¿Quíen <span class="text-amber-300">soy</span>?',
-        'AboutMe.Description':
-            'Soy un desarrollador de software apasionado por crear herramientas útiles y eficientes. Amo aprender nuevas tecnologías y formas de optimizar mis soluciones.',
-        'AboutMe.WhatILikeToDo':
-            'Tengo experiencia tanto en el ambito web, como en el desarrollo de aplicaciones de escritorio y móviles. Y es gracias a eso que he decubierto que amo ser polivalente, difruto tanto del trabajo frontend, aplicando mis conocimientos de diseño UI/UX adquiridos con la experiencia, como del desarrollo backend, creando APIs robustas y escalables.',
-        'Contact.Title': 'Formas de contacto',
-        'Contact.Social.Title': 'Támbien mis redes',
-    },
+  en: {
+    "General.DownloadCv": "Download CV",
+    "General.EmailCopied": "Email copied to clipboard!",
+    "General.EmailCopyError": "Failed to copy email.",
+    "Hero.Title":
+      'Welcome to my portfolio!\nIm <b class="font-bold">{name}</b>, <span class="text-amber-300 font-medium">non just any</span> software developer.',
+    "Hero.Gotoblog": "Try my blog",
+    "AboutMe.Title": 'Some things about <span class="text-accent">me</span>',
+    "AboutMe.Description":
+      "I'm a passionate software developer who loves creating useful and efficient tools. I enjoy learning new technologies and finding ways to optimize my solutions.",
+    "AboutMe.WhatILikeToDo":
+      "I have experience in both web development and desktop/mobile application development. This versatility has helped me discover my love for being a multi-disciplinary developer. I enjoy working on the frontend, applying my UI/UX design knowledge gained through experience, as well as backend development, creating robust and scalable APIs.",
+    "Tech.Title": "Technologies I've used",
+    "Tech.Placeholder": "Search technologies...",
+    "Project.Title": "Some of my projects",
+    "Project.Label.1": "Repository Link",
+    "Project.Label.2": "Live Demo",
+    "Contact.Title": "Ways of Contact",
+    "Contact.Social.Title": "And my socials",
+    "Blog.Nav.Return": "Return to my portfolio",
+    "Blog.Nav.Board": "Board",
+  },
+  es: {
+    "General.DownloadCv": "Descarga mi CV",
+    "General.EmailCopied": "¡Email copiado al portapapeles!",
+    "General.EmailCopyError": "Error al copiar el email.",
+    "Hero.Title":
+      "Bienvenido a mi portafolio.\n<b class='font-bold'>{name}</b>: un <span class='text-accent font-medium'>apasionado</span> del software.",
+    "Hero.Gotoblog": "Dale un vistazo blog",
+    "AboutMe.Title": '¿Quíen <span class="text-amber-300">soy</span>?',
+    "AboutMe.Description":
+      "Soy un desarrollador de software apasionado por crear herramientas útiles y eficientes. Amo aprender nuevas tecnologías y formas de optimizar mis soluciones.",
+    "AboutMe.WhatILikeToDo":
+      "Tengo experiencia tanto en el ambito web, como en el desarrollo de aplicaciones de escritorio y móviles. Y es gracias a eso que he decubierto que amo ser polivalente, difruto tanto del trabajo frontend, aplicando mis conocimientos de diseño UI/UX adquiridos con la experiencia, como del desarrollo backend, creando APIs robustas y escalables.",
+    "Tech.Title": "Tecnologías que controlo",
+    "Tech.Placeholder": "Buscar tecnologías...",
+    "Project.Title": "Alguno de mis proyectos",
+    "Project.Label.1": "Enlace al repositorio",
+    "Project.Label.2": "Demo en vivo",
+    "Contact.Title": "Formas de contacto",
+    "Contact.Social.Title": "Támbien mis redes",
+    "Blog.Nav.Return": "Mi Portafolio",
+    "Blog.Nav.Board": "Tablón",
+  },
 } as const;
 
 export function getLangFromURL(url: URL) {
-    const pathParts = url.pathname.split('/').filter(Boolean);
-    
-    // Buscar 'es' o 'en' en las partes del path
-    // Esto funciona tanto para /es/ como para /portfolio/es/
-    const lang = pathParts.find(part => part in ui);
-    
-    if (lang && lang in ui) return lang as keyof typeof ui;
-    return defaultLanguage;
+  const pathParts = url.pathname.split("/").filter(Boolean);
+
+  // Buscar 'es' o 'en' en las partes del path
+  // Esto funciona tanto para /es/ como para /portfolio/es/
+  const lang = pathParts.find((part) => part in ui);
+
+  if (lang && lang in ui) return lang as keyof typeof ui;
+  return defaultLanguage;
 }
 
 export function useTranslation(lang: keyof typeof ui) {
-    return function t(
-        key: keyof (typeof ui)[typeof defaultLanguage],
-        params?: Record<string, any>
-    ): string {
-        let translation: string = ui[lang][key] || ui[defaultLanguage][key];
+  return function t(
+    key: keyof (typeof ui)[typeof defaultLanguage],
+    params?: Record<string, any>,
+  ): string {
+    let translation: string = ui[lang][key] || ui[defaultLanguage][key];
 
-        if (params) {
-            translation = injectParamsIntoString(translation, params);
-        }
+    if (params) {
+      translation = injectParamsIntoString(translation, params);
+    }
 
-        return translation;
-    };
+    return translation;
+  };
 }
 
 export function injectParamsIntoString(
-    str: string,
-    params: Record<string, any>
+  str: string,
+  params: Record<string, any>,
 ): string {
-    return str.replace(/\{(\w+)\}/g, (_, paramKey) => {
-        return params[paramKey] ?? `{${paramKey}}`;
-    });
+  return str.replace(/\{(\w+)\}/g, (_, paramKey) => {
+    return params[paramKey] ?? `{${paramKey}}`;
+  });
 }
